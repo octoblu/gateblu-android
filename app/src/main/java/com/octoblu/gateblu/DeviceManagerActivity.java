80a03a3f6eec4907916f1b7785d96e5f608eed7a
redaphid <iam@aaronherres.com>
2014-12-16 17:50:49 -0700

    Well, it doesn't crash... so that's something.

--- app/src/main/java/com/octoblu/gateblu/DeviceManagerActivity.java
+++ app/src/main/java/com/octoblu/gateblu/DeviceManagerActivity.java
@@ -2,19 +2,68 @@ package com.octoblu.gateblu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
 
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-/**
- * Created by redaphid on 12/16/14.
- */
-public class DeviceManagerActivity extends Activity implements CordovaInterface {
+public class DeviceManagerActivity extends ActionBarActivity implements CordovaInterface {
+
+    private final List<Device> devices = new ArrayList<>();
+    private final ExecutorService threadPool = Executors.newCachedThreadPool();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gateblu);
+        addDevices();
+
+        DeviceGridAdapter adapter = new DeviceGridAdapter(getApplicationContext(), devices);
+
+        GridView gridView = (GridView)findViewById(R.id.devices_grid);
+        gridView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_gateblu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addDevices() {
+        devices.add(new Device("Blink(1)", "device:blink1"));
+        devices.add(new Device("Hue", "device:hue"));
+    }
+
     @Override
     public void startActivityForResult(CordovaPlugin cordovaPlugin, Intent intent, int i) {
-
+        //I don't know what this is!
     }
 
     @Override
@@ -24,16 +73,17 @@ public class DeviceManagerActivity extends Activity implements CordovaInterface 
 
     @Override
     public Activity getActivity() {
-        return null;
+        return this;
     }
 
     @Override
     public Object onMessage(String s, Object o) {
+        Log.i("DeviceManager", s);
         return null;
     }
 
     @Override
     public ExecutorService getThreadPool() {
-        return null;
+        return threadPool;
     }
 }
